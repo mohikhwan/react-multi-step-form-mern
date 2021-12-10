bb98cbdd04178301cbfea680ac0f7cb42f46897a
Ikhwan <ikhwanrts@gmail.com>
2021-12-10 21:48:20 +0700

    Create login function

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -4,6 +4,7 @@ import Header from '../components/Header';
 import FirstStep from '../components/FirstStep';
 import SecondStep from '../components/SecondStep';
 import ThirdStep from '../components/ThirdStep';
+import Login from '../components/Login';
 
 
 const AppRouter = () => {
@@ -45,6 +46,7 @@ const AppRouter = () => {
             )}
             path="/third"
           />
+          <Route component={Login} path="/login" />
         </Switch>
       </div>
     </BrowserRouter>
