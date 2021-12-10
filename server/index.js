e3f6553912df8a22f39c2bc11bf940a15f027b01
Ikhwan <ikhwanrts@gmail.com>
2021-12-10 21:58:13 +0700

    Remove CORS library

--- server/index.js
+++ server/index.js
@@ -1,13 +1,15 @@
+const path = require('path');
 const express = require('express');
 const userRouter = require('./routers/user');
-const cors = require('cors');
+// const cors = require('cors');
 require('./db');
 
 const app = express();
 const PORT = process.env.PORT || 3030;
 
+app.use(express.static(path.join(__dirname, '..', 'build')));
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
 app.use(userRouter);
 
 app.get('/', (req, res) => {
